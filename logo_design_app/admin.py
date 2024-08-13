7e38916ea4041353c0423507ba95c74c0babf53e
taewoo-dev <twin9774@gmail.com>
2024-08-13 23:55:57 +0900

    feat: Post, Comment models added

--- logo_design_app/admin.py
+++ logo_design_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from logo_design_app.models import Post, Comment
+
 
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Comment)
+class Admin(admin.ModelAdmin):
+    pass
