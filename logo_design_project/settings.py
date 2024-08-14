00abb37506d9d32c66bba0ed8e3bfa813edfe62f
taewoo-dev <twin9774@gmail.com>
2024-08-14 16:44:34 +0900

    feat: Post model thumbnail Field migrate

--- logo_design_project/settings.py
+++ logo_design_project/settings.py
@@ -17,6 +17,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = BASE_DIR / "templates"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
