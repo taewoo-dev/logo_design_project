847f25823a1b9e91c466a76add8669950d8085ab
taewoo-dev <twin9774@gmail.com>
2024-08-19 21:47:53 +0900

    feat: review dynamic page added

--- logo_design_app/views.py
+++ logo_design_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 
-from logo_design_app.models import Post, Comment, Survey
+from logo_design_app.models import Post, Comment, Survey, Review
 
 
 # Create your views here.
@@ -30,6 +30,7 @@ def post_detail(request, post_id):
 def post_add(request):
     if request.method == "POST":
         print("method POST")
+        print(request.FILES)
         title = request.POST["title"]
         content = request.POST["content"]
         thumbnail = request.FILES["thumbnail"]
@@ -60,7 +61,12 @@ def column(request):
 
 
 def review(request):
-    return render(request, "main/review.html")
+    reviews = Review.objects.all()
+    print(reviews)
+    contents = {
+        "reviews": reviews
+    }
+    return render(request, "main/review.html", contents)
 
 
 def notice(request):
@@ -68,25 +74,34 @@ def notice(request):
 
 
 def survey_form(request):
-    print(request.POST)
-    username = request.POST["UserName"]
-    phone_number = request.POST["Phone"]
-    email = request.POST["Email"]
-    brand_name = request.POST["BrandName"]
-    brand_mean = request.POST["BrandMean"]
-    brand_desc = request.POST["BrandDesc"]
-    brand_target = request.POST["BrandTarget"]
-    logo_type = request.POST["radio"]
-    brand_image = request.POST["BrandPlusImage"]
-    brand_color = request.POST["BrandColor"]
-    Survey.objects.create(username=username, phone=phone_number, email=email, brand_name=brand_name,
-                          brand_mean=brand_mean, brand_desc=brand_desc, brand_target=brand_target, logo_type=logo_type,
-                          brand_image=brand_image, brand_color=brand_color)
-    print("새로운 고객 추가")
+    if request.method == "Post":
+        print(request.POST)
+        username = request.POST["UserName"]
+        phone_number = request.POST["Phone"]
+        email = request.POST["Email"]
+        brand_name = request.POST["BrandName"]
+        brand_mean = request.POST["BrandMean"]
+        brand_desc = request.POST["BrandDesc"]
+        brand_target = request.POST["BrandTarget"]
+        logo_type = request.POST["radio"]
+        brand_image = request.POST["BrandPlusImage"]
+        brand_color = request.POST["BrandColor"]
+        Survey.objects.create(username=username, phone=phone_number, email=email, brand_name=brand_name,
+                              brand_mean=brand_mean, brand_desc=brand_desc, brand_target=brand_target, logo_type=logo_type,
+                              brand_image=brand_image, brand_color=brand_color)
+        print("새로운 고객 추가")
     # 견적서를 파일로 변환하는 로직 이후에 추가
     return redirect("/main")
 
 
 def review_form(request):
-    print(request.POST)
-    pass
+    if request.method == "POST":
+        print(request.POST)
+        content = request.POST["ReviewContent"]
+        rating = request.POST["star"][0]
+        print(request.FILES)
+        image = request.FILES['ReviewThumbnail']
+        Review.objects.create(content=content, rating=rating, thumbnail=image)
+        print("새로운 리뷰 추가")
+    return redirect("/review")
+
